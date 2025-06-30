f0a1b5e88f4cf34e1c4853162c253f08a9d37721
Luis Morua <lfdomc@gmail.com>
2025-06-29 21:46:11 -0600

    4

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,27 +7,25 @@ import { logo } from "@/clientdata/clientInfo";
 
 // Styled Components
 const HeaderContainer = styled.header`
-  position: fixed; // Hace que el header sea fijo
-  top: 0; // Asegura que esté en la parte superior
-  left: 0; // Asegura que comience desde el borde izquierdo
-  right: 0; // Asegura que cubra todo el ancho
-  z-index: 1000; // Asegura que esté por encima de otros elementos
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 1rem 2rem;
   background-color: #ffffff;
-  color: #ffffff;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 `;
-
 
 const NavMenu = styled.nav`
   display: flex;
   gap: 1.5rem;
 
   @media (max-width: 768px) {
-    display: none; // Ocultar menú en pantallas pequeñas
+    display: none;
   }
 `;
 
@@ -51,7 +49,7 @@ const MobileMenuButton = styled.button`
   display: none;
 
   @media (max-width: 768px) {
-    display: block; // Mostrar botón en pantallas pequeñas
+    display: block;
   }
 `;
 
@@ -90,8 +88,7 @@ const Header = () => {
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      // Verifica si el clic ocurrió fuera del menú y fuera del botón del menú
-      const target = event.target as HTMLElement; // Convertimos el tipo a HTMLElement
+      const target = event.target as HTMLElement;
       if (
         isMobileMenuOpen &&
         !target.closest(".mobile-menu") &&
@@ -101,10 +98,8 @@ const Header = () => {
       }
     };
 
-    // Agrega el evento al documento
     document.addEventListener("click", handleClickOutside);
 
-    // Limpia el evento cuando el componente se desmonta
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
@@ -115,18 +110,14 @@ const Header = () => {
       {/* Encabezado */}
       <HeaderContainer>
         {/* Logo envuelto en un Link para redirigir a la página de inicio */}
-        <Link href="/" passHref>
+        <Link href="/" aria-label="Inicio">
           <Image
             src={logo}
             alt="Logo"
-            width={220} // Define el ancho
-            height={220} // Define la altura
-            priority={true}
-            style={{
-              objectFit: "contain", // Asegura que la imagen mantenga su relación de aspecto
-              width: "auto", // Asegura que el ancho se ajuste automáticamente si es necesario
-              height: "80px", // Altura fija para el logo
-            }}
+            width={220}
+            height={80}
+            priority
+            style={{ objectFit: "contain" }}
           />
         </Link>
 
@@ -144,6 +135,7 @@ const Header = () => {
         <MobileMenuButton
           className="mobile-menu-button"
           onClick={toggleMobileMenu}
+          aria-label="Menú móvil"
         >
           ☰
         </MobileMenuButton>
